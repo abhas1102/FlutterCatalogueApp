c54a82d61de690b1d111108129495b0a3a68f6a1
Abhas Kumar <abhas1102@gmail.com>
2022-12-12 17:11:19 +0530

    Day-2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {  
+    /*int days = 30; // For int value
+    String name = "Codepur"; //For string value
+    double pi = 3.14; // For double value 
+    bool isMale = true; // For boolean value
+    num temperature = 30.5 // For any type of number either it's int or double
+
+    var day = "Tuesday" // Variable value and no need to define type. It does smartcast
+    const pi = 3.14 // Fixed value that is not going to change. It also does smartcast */
+    
     return MaterialApp(
       
-      home:Material(
-        child:Center(
-         child: Container(
-          child: Text("Welcome to 30 Days of Flutter"),
-      ),
-
-        ) , 
-      ),
+      home:HomePage(),
     );
   }
 }
